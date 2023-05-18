4c4d1a682558e134e26d0e6e25bee1089bf1d670
httpsAshes <ashtonjackson705@gmail.com>
2023-05-18 13:30:18 +0200

    Services added to client and server

---
+++ BlazorEcommerce/Client/Services/ProductServices/ProductService.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace BlazorEcommerce.Client.Services.ProductServices
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _http;
+
+        public ProductService(HttpClient http)
+        {
+            _http = http;
+        }
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public async Task GetProducts()
+        {
+            var result = 
+                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/producta");
+            if(result != null && result.Data !=null)
+                Products = result.Data;
+
+        }
+    }
+}
---
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorEcommerce.Server.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly DataContext _context;
+        public ProductService(DataContext contextt)
+        {
+            _context = contextt;
+            
+        }
+        public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
+        {
+            var response = new ServiceResponse<List<Product>>
+            {
+                Data = await _context.Products.ToListAsync()
+            };
+            return response;
+        }
+    }
+}
