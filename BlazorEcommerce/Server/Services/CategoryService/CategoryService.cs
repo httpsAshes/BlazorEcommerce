746082d9ae1122263fe71c9a1fdc6b8e1ccb3a99
httpsAshes <ashtonjackson705@gmail.com>
2023-05-22 10:44:54 +0200

    Added Category

---
+++ BlazorEcommerce/Server/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorEcommerce.Server.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly DataContext _Context;
+        public CategoryService(DataContext context)
+        {
+            _Context = context;
+        }
+        public  async Task<ServiceResponse<List<Category>>> GetCategories()
+        {
+           var categories = await _Context.Categories.ToListAsync();
+           return new ServiceResponse<List<Category>> 
+           {
+               Data = categories 
+           };
+
+        }
+    }
+}
