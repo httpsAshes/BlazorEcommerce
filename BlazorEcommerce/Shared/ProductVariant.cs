c1a2c6ccefe992cfe7259ac9f8c48ff1733e861c
httpsAshes <ashtonjackson705@gmail.com>
2023-05-23 15:30:57 +0200

    Select Variant controls and

--- BlazorEcommerce/Shared/ProductVariant.cs
+++ BlazorEcommerce/Shared/ProductVariant.cs
@@ -15,7 +15,7 @@ namespace BlazorEcommerce.Shared
         public Product Product { get; set; }
         public int ProductId { get; set; }
         public ProductType ProductType { get; set; }
-        public int? ProductTypeId { get; set; }
+        public int ProductTypeId { get; set; }
 
         [Column(TypeName = "decimal(18,2")]
         public decimal Price { get; set; }
